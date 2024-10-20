e6a4d64f50eab59849e3d43bf9d6ea587318ac6f
ELI-THE_CREATOR <neverlookbackeli@gmail.com>
2024-10-20 22:57:51 +0100

    added public path Webpack

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,6 +9,7 @@ const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
+        publichPath: '/container/latest/'
     },
     plugins: [
         new ModuleFederationPlugin({
